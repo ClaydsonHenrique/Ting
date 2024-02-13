55ae2cb34a692c31a09fe1c7da8bc595a00e8ef8
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-13 16:18:47 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if path_file.split(".")[-1] != "txt":
+        return None
+
+    try:
+        with open(path_file, "r") as file:
+            lines = file.readline()
+            return lines
+    except FileNotFoundError:
+        return None
