30cce31fc9464db9a6fdb5acb72d34619d0c5f2c
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-15 17:21:18 -0300

    finalizando requisito 2 e 3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,11 +1,22 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
-    if path_file.split(".")[-1] != "txt":
+    if not path_file.split(".")[-1] == "txt":
+        print(
+            "Formato inválido",
+            file=sys.stderr,
+        )
         return None
 
     try:
-        with open(path_file, "r") as file:
-            lines = file.readline()
+        with open(path_file, "r") as arquivo:
+            lines = [line.rstrip("\n") for line in arquivo.readlines()]
             return lines
     except FileNotFoundError:
+        print(
+            f"Arquivo {path_file} não encontrado",
+            file=sys.stderr,
+        )
         return None
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,34 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+
+    name = path_file
+
+    for names in instance.queue:
+        if names.get("nome_do_arquivo") == name:
+            print(
+                f"O arquivo {name} já foi processado anteriormente.",
+                file=sys.stdout,
+            )
+            return
+
+    lines = txt_importer(path_file)
+    if lines is None:
+        print(f"Falha ao processar o arquivo {name}.", file=sys.stdout)
+        return
+
+    result = {
+        "nome_do_arquivo": name,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    # Adiciona os dados do resultado na instância de Processamento
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
 
 
 def remove(instance):
